da23f2c0ea1264b2fdb57127323f7a0d4f4e4479
rohan <rohanrajgautam@gmail.com>
2020-10-21 14:23:03 +0545

    added seperate module for chatbot

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -1,13 +1,4 @@
-const dialogflow = require('dialogflow');
-const config = require('../config/keys');
-
-const sessionClient = new dialogflow.SessionsClient();
-
-const sessionPath = sessionClient.sessionPath(
-  config.googleProjectID,
-  config.dialogFlowSessionID
-);
-
+const chatbot = require('../chatbot/chatbot');
 
 module.exports = (app) => {
   app.get('/', (req, res) => {
@@ -15,32 +6,8 @@ module.exports = (app) => {
   });
 
   app.post('/api/df_text_query', async (req, res) => {
-    // The text query request.
-    const request = {
-      session: sessionPath,
-      queryInput: {
-        text: {
-          // The query to send to the dialogflow agent
-          text: req.body.text,
-          // The language used by the client (en-US)
-          languageCode: config.dialogFlowSessionLanguageCode,
-        },
-      },
-    };
-
-    // Send request and log result
-    let responses = await sessionClient.detectIntent(request);
-    console.log('Detected intent');
-    const result = responses[0].queryResult;
-    console.log(`  Query: ${result.queryText}`);
-    console.log(`  Response: ${result.fulfillmentText}`);
-    if (result.intent) {
-      console.log(`  Intent: ${result.intent.displayName}`);
-    } else {
-      console.log(`  No intent matched.`);
-    }
-
-    res.send(result);
+    let responses = await chatbot.textQuery(req.body.text, req.body.parameters);
+    res.send(responses[0].queryResult);
   });
 
   app.post('/api/df_event_query', (req, res) => {
