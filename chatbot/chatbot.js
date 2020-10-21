465b8089ea29163cc4ed1bbafe63c36857f93470
rohan <rohanrajgautam@gmail.com>
2020-10-21 14:59:02 +0545

    config local and server heroku env varaibles

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -4,7 +4,14 @@ const dialogflow = require('dialogflow');
 const structjson = require('./structjson');
 const config = require('../config/keys');
 
-const sessionClient = new dialogflow.SessionsClient();
+const projectID = config.googleProjectID;
+
+const credentials = {
+  client_email: config.googleClientEmail,
+  private_key: config.googlePrivateKey,
+};
+
+const sessionClient = new dialogflow.SessionsClient({ projectID, credentials });
 
 const sessionPath = sessionClient.sessionPath(
   config.googleProjectID,
