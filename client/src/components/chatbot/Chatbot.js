156bc87fcf91a0451f73eafe5a4bf3820e3b2be5
rohan <rohanrajgautam@gmail.com>
2020-10-22 15:13:00 +0545

    adding API calls to backend app

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,8 +1,61 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import axios from 'axios';
 // import Message from './Message';
 // import Card from './Card';
 
-const Chatbot = () => <div>Chatbot</div>;
+
+const Chatbot = () => {
+  const [messages, setMessages] = useState([]);
+
+  async df_text_query(text) {
+    let says = {
+        speaks: 'me',
+        msg: {
+            text: {
+                text: text
+            }
+        }
+    }
+    
+    setMessages({messages: [...messages, says]})
+    const res = await axios.post('/api/df_text_query', {text})
+
+    for(let msg of res.data.fulfillmentMessages) {
+      says = {
+        speaks: 'bot',
+        msg
+      }
+      setMessages({messages: [...messages, says]})
+    }
+  }
+
+  async df_event_query(event) {
+    const res = await axios.post('/api/df_event_query', {event});
+
+    for(let msg of res.data.fulfillmentMessages){
+      let says = {
+        speaks: 'me',
+        msg
+      }
+      setMessages({messages: [...messages, says]})
+    }
+  }
+
+  return (
+    <div style={{ height: 400, width: 400, float: 'right' }}>
+        <div
+        id='chatbot'
+        style={{
+            height: '100%',
+            width: '100%',
+            overflow: 'auto',
+        }}
+        >
+        <h2>Chatbot</h2>
+        <input type='text' />
+        </div>
+    </div>
+  )
+};
 
 export default Chatbot;
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
-import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'materialize-css/dist/css/materialize.min.css';
 import App from './components/App';
+import './assets/app.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
